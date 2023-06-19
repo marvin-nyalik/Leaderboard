9b1d558e2ad3369036e81a516c1452c568b1d72d
MARVIN <o.silverygold@gmail.com>
2023-06-19 17:44:28 +0300

    Add project files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import _ from 'lodash';
 import './style.css';
+import populateScores from './modules/populateScores.js';
+import { submitScore } from './modules/domElements.js';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+submitScore.addEventListener('click', populateScores);
