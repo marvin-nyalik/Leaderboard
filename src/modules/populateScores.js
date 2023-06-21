1f803b21c543b4df8613d56210217df33c6e38a5
MARVIN <o.silverygold@gmail.com>
2023-06-21 17:55:18 +0300

    Update the UI

--- src/modules/populateScores.js
+++ src/modules/populateScores.js
@@ -4,10 +4,17 @@ import { getGameScores } from './gameAPI.js';
 const populateScores = async () => {
   const allResponses = await getGameScores();
   scoreList.innerHTML = '';
-  allResponses.forEach((scr) => {
+  allResponses.forEach((scr, index) => {
     const listItem = `
         <li class="list-item"> 
-          <span class="user-name"> ${scr.user}</span>
+          <span class="user-name" id="user-${index}">
+          ${index + 1}. 
+          ${index === 0 ? scr.user.toUpperCase() : scr.user} 
+          ${index === 0 ? `<i class='bx bxs-crown'></i>` : 
+          index === 1 ? `<i class='bx bxs-star'></i>` : 
+          index ===2 ? `<i class='bx bx-star'></i>`:
+          ``}
+          </span>
           <span class="user-score"> ${scr.score} </span>
         </li>
         `;
